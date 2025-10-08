aa5eff62ea9b4aa0c0e0aad985ba22d4d7425f15
Tesla Junior <teslajunior0552@gmail.com>
2025-10-08 10:51:37 +0000

    Update Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     return(
         <header>
         <nav>
-            <img src="./globe.png" alt="globe png" />
+            <img src="globe.png" alt="globe png" />
             <span>My travel journal</span>
         </nav>
     </header>
@@ -11,3 +11,4 @@ export default function Header(){
     
 
 }
+
