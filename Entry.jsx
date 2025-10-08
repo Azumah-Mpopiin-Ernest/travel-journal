f69174034127c93a9625dfa1fc5a962fa1e179bd
Tesla Junior <teslajunior0552@gmail.com>
2025-10-08 10:39:06 +0000

    Add files via upload

---
+++ Entry.jsx
@@ -0,0 +1,23 @@
+
+
+export default function Entry(props){
+    return(
+        
+        <main className="first-country-container">
+            <img className="fuji-png" src={props.img.src} alt={props.img.alt} />
+            <div className="country-and-description-container">
+                <span className="marker-googlemap-container">
+                <img src="./images/marker.png" alt="marker image" />
+                <p>{props.country}</p>
+                <small>
+                    <a href={props.googleMapsLink} target="blank">View on google maps</a>
+                </small>
+                </span>
+                <h2 className="h2">{props.title}</h2>
+                <p className="p1">{props.dates}</p>
+                <p className="p2">{props.text}</p>
+                
+            </div>
+        </main>
+    )
+}
---
+++ Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header(){
+    return(
+        <header>
+        <nav>
+            <img src=".\images\globe.png" alt="globe png" />
+            <span>My travel journal</span>
+        </nav>
+    </header>
+    )
+    
+}
